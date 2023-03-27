f4a1e3242a518f3396e4bfc407c57d62d1c23e86
Lourens-Spiced <lourensvdzee@gmail.com>
2023-03-27 18:13:52 +0200

    show answer first part done

--- js/index.js
+++ js/index.js
@@ -38,13 +38,14 @@ function bookmarkEmpty() {
 
 // Bookmark Icon toggle
 const bookmarkIcons = document.querySelectorAll('[data-js="card_bookmark"]');
-console.log(bookmarkIcons.length);
+// console.log(bookmarkIcons.length);
 
 for (let i = 0; i < bookmarkIcons.length; i++) {
     const bookmarkIcon = bookmarkIcons[i];
     console.log(bookmarkIcon);
 
     bookmarkIcon.addEventListener("click", () => {
+        console.log(bookmarkIcon);
         if (bookmarkIcon.src.match("./assets/bookmark.png")) {
             bookmarkIcon.src = "./assets/bookmark_filled.png";
         } else {
@@ -52,4 +53,43 @@ for (let i = 0; i < bookmarkIcons.length; i++) {
         }
     });
 }
+
+// Answer toggle
+const answerButtons = document.querySelectorAll('[data-js="answer-button"]');
+const answerToggles = document.querySelectorAll('[data-js="answer-text"]');
+// console.log(answerToggles.length);
+
+for (let i = 0; i < answerButtons.length; i++) {
+    const answerButton = answerButtons[i];
+    const answerToggle = answerToggles[i];
+    console.log(answerButton);
+
+    answerButton.addEventListener("click", () => {
+        if (answerToggle.style.display == "none") {
+            answerToggle.style.display = "block";
+        } else {
+            answerToggle.style.display = "none";
+        }
+    });
+}
+
+// Answer text
+/* const buttonTexts = document.querySelectorAll('[data-js="button-text"]');
+console.clear();
+// console.log(buttonText);
+
+for (let i = 0; i < buttonTexts.length; i++) {
+    const answerButton = answerButtons[i];
+    const buttonText = buttonTexts[i];
+    console.log(buttonTexts);
+
+    answerButton.addEventListener("click", () => {
+        if (buttonText.style.value == "Show Answer") {
+            answerToggle.style.value = "Hide Answer";
+        } else {
+            answerToggle.style.display = "none";
+        }
+    });
+} */
+
 
